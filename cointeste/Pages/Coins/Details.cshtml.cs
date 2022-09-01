051ff2f69c0c3b6fb13ca5c1eaecc6c3dd63587e
Fausto Junior <faustosargacojr@hotmail.com>
2022-08-31 21:47:08 -0300

    Add project files.

---
+++ cointeste/Pages/Coins/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using cointeste.Data;
+using cointeste.Models;
+
+namespace cointeste.Pages.Coins
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly cointeste.Data.cointesteContext _context;
+
+        public DetailsModel(cointeste.Data.cointesteContext context)
+        {
+            _context = context;
+        }
+
+      public Coin Coin { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Coin == null)
+            {
+                return NotFound();
+            }
+
+            var coin = await _context.Coin.FirstOrDefaultAsync(m => m.Id == id);
+            if (coin == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Coin = coin;
+            }
+            return Page();
+        }
+    }
+}
