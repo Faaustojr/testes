1779ac820bd86c66ee4aa82fbf2f8d8b23347e8b
Fausto Junior <95933729+Faaustojr@users.noreply.github.com>
2022-09-05 16:36:27 -0300

    Delete cointeste directory

--- cointeste/Models/Coin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace cointeste.Models
-{
-    public class Coin
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
